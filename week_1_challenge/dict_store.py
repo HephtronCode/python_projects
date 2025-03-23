5d4825eb3e086fec87ec2ef7e4e969ce69052747
HephtronCode <bakr.innovator@gmail.com>
2025-03-23 07:43:18 +0100

    added a new python test

---
+++ week_1_challenge/dict_store.py
@@ -0,0 +1,13 @@
+person_info = {}
+
+user_name = input("What is your name? ")
+user_age = input("How old are you? ")
+user_city = input("Where do you live? ")
+user_color = input("What is your favorite color? ")
+
+person_info ["Name"] = user_name
+person_info ['Age'] = user_age
+person_info ["City"] = user_city
+person_info ["Color"] = user_color
+
+print(person_info)
---
+++ week_1_challenge/tuple_loop.py
@@ -0,0 +1,5 @@
+favorite_books = ("Harry Potter", "The Hobbit", "The Lord of the Rings", "The Alchemist", "The Da Vinci Code")
+
+print("My favorite books are: ")
+for book in favorite_books:
+    print(f'{book}\n')
